30509ffe8f84c6c5e74c0321d3cacf837ca3e297
pavel <pavel.vjalicin@softar.uk>
2020-06-17 04:08:59 +0100

    Change TopNav to use tabs.

--- src/js/components/TopNav.jsx
+++ src/js/components/TopNav.jsx
@@ -3,21 +3,44 @@ import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Button from "@material-ui/core/Button";
 import {Link} from "react-router-dom";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
 
 class TopNav extends Component {
     constructor(props) {
         super(props)
+        this.handleChange = this.handleChange.bind(this)
+        this.setActiveTab = this.setActiveTab.bind(this)
+        this.hrefs = this.props.links.map(x => x[0])
+        this.state = {
+            activeTab:this.setActiveTab(window.location.pathname)
+        }
+
+    }
+
+    setActiveTab(activeTab) {
+        return this.hrefs.includes(activeTab) ? activeTab : false
+    }
+
+    handleChange(e,activeTab) {
+        this.setState({
+            activeTab:this.setActiveTab(activeTab)
+        })
     }
 
     render() {
         return <AppBar color={'default'} position={"fixed"}>
-
             <Toolbar>
-                {this.props.links.map(link =>
-                    <Button key={link[0]} component={Link} to={link[0]}>{link[1]}</Button>
-                )}
+                <Tabs value={this.state.activeTab} onChange={this.handleChange}>
+                    {this.props.links.map(link =>
+                        <Tab key={link[0]}
+                             label={link[1]}
+                             component={Link}
+                             value={link[0]}
+                             to={link[0]}/>
+                    )}
+                </Tabs>
             </Toolbar>
-
         </AppBar>
     }
 }
