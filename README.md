ad3502115ff81160d84c9a0cd7a92d4f8885646e
pavel <pavel.vjalicin@softar.uk>
2020-07-02 21:00:33 +0100

    Web-site optimization. Transitioning from express to Napi.

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 Personal website for Pavel Vjalicin
+
+NodeJS 12 required
+
+Create private/public keys for HTTP2 connection in during dev:
+openssl req -x509 -newkey rsa:4096 -nodes -sha256 -subj '/CN=localhost' -keyout private.pem -out cert.pem
--- server.js
+++ server.js
@@ -1,13 +1,131 @@
 const validateEmail = require("./src/js/validateEmail").default
 const Express = require("express")
+const http2 = require("http2")
 const path = require("path")
-const fs = require('fs');
+const fs = require('fs')
 const fetch = require('node-fetch');
 const sendEmail = require('./email').default
 const bodyParser = require('body-parser')
+const Hapi = require("@hapi/hapi")
+
+const reposDir = this.dataDir+"/repos.json"
+let repos = {
+    lastUpdatedEpoch:0,
+    data:[]
+};
+const favicon = fs.readFileSync("./favicon.ico")
+
+function updateRepo() {
+    console.log("GitHub repos updated")
+    return fetch("https://api.github.com/users/PavelVjalicin/repos")
+        .then(resp => {
+            if (resp.ok) return resp
+            else throw "GitHub server error"
+        })
+        .then(resp => resp.json())
+        .then(json => {
+            if (fs.existsSync(reposDir)) fs.unlinkSync(reposDir)
+            json.sort((a, b) => new Date(b.updated_at) - new Date(a.updated_at))
+            const resp = {lastUpdatedEpoch: Date.now(), data: json}
+            fs.writeFile(reposDir, JSON.stringify(resp), (err) => {
+            })
+            return resp
+        })
+}
+
+async function getRepos() {
+    if(repos.data.length === 0) {
+        if(fs.existsSync(reposDir))  {
+            //Retrieve from cache
+            const json = JSON.parse(fs.readFileSync(reposDir));
+            if(json.lastUpdatedEpoch + (2*60*60*1000) < Date.now() ) {
+                return await updateRepo()
+            } else return json
+        } else {
+            return await updateRepo()
+        }
+
+    } else if(repos.lastUpdatedEpoch + (2*60*60*1000) < Date.now() ) {
+        //Update
+        return await updateRepo()
+    } else {
+        //Return in memory
+        return repos
+    }
+}
+
+const initHapi = async () => {
+    const listener = http2.createSecureServer(
+        {
+            key:fs.readFileSync("private.pem"),
+            cert:fs.readFileSync("cert.pem")
+        }
+    )
+
+    const server = Hapi.Server({
+        listener,
+        port: "9000",
+        routes: {
+            files: {
+                relativeTo: __dirname
+            }
+        }
+    })
+
+    await server.register(require("@hapi/inert"))
+
+    server.route([{
+        method:'GET',
+        path:"/api/repo",
+        handler:(req,h) => {
+            return getRepos().then(r => {
+                repos = r
+                return JSON.stringify(repos.data)
+            })
+        }
+    },{
+        method:'GET',
+        path:"/dist/{any*}",
+        handler: (req,h) => {
+            const type = req.params.any.split(".").pop()
+
+            let resp = h.file("dist/"+req.params.any+".gz").header('Content-Encoding', 'gzip')
+
+            if(type === "js") resp.header("Content-Type","text/javascript")
+            else if(type === "css") resp.header("Content-Type","text/css")
+
+            return resp
+        }
+    },{
+        method:'GET',
+        path:'/favicon.ico',
+        handler: (req,h) =>
+            h.response(favicon).header('Content-Length', favicon.length)
+                .header('Content-Type', 'image/x-icon')
+                .header("Cache-Control", "public, max-age=2592000") //Expires in a month
+                .header("Expires", new Date(Date.now() + 2592000000).toUTCString())
+
+    },{
+        method:'GET',
+        path:'/{any*}',
+        handler: (req,h) => {
+            return h.file("src/index.html")
+        }
+    }])
+
+    await server.start(err => {
+        if(err) console.log(err)
+    })
+
+    console.log('Server running at:', server.info.uri);
+}
+
 class Server {
     constructor(_port,isDev) {
         this.port = _port
+
+        initHapi()
+
         this.app = new Express()
         this.sockets = []
         this.isDev = isDev
@@ -44,7 +162,13 @@ class Server {
         this.app.get('*.js', gz("text/javascript"));
         this.app.get('*.css', gz("text/css") );
 
+        this.app.get("/robots.txt",(req,res) => {
+            res.sendFile("robots.txt",appOptions)
+        })
+
         this.app.use("/dist", Express.static('dist'))
+
+        this.app.use("/public", Express.static('public'))
 
         if(isDev) {
             this.app.ws("/", (ws,req) => {
@@ -72,16 +196,15 @@ class Server {
                     .then(
                         res.sendStatus(200)
                     ).catch( e => {
-                        console.log(e)
-                        res.status(500).json({error:"Something went wrong"})
-                    })
+                    console.log(e)
+                    res.status(500).json({error:"Something went wrong"})
+                })
             } else {
                 res.status(500).json({error:"Form was not filled in correctly."})
             }
         })
 
         this.app.get("/favicon.ico", (req, res) => {
-
             res.statusCode = 200;
             res.setHeader('Content-Length', favicon.length);
             res.setHeader('Content-Type', 'image/x-icon');
