0cbbf3e94fdb3ffdd9f28790b9489db8e03ed466
Pavel Vjalicin <pavelVjalicin@users.noreply.github.com>
2022-05-17 21:25:49 +0100

    Updated experience

--- src/js/components/pages/experience/Experience.jsx
+++ src/js/components/pages/experience/Experience.jsx
@@ -1,11 +1,11 @@
-import React, {Component} from "react"
-import {LineAnimation} from "./LineAnimation";
-import {PageTitle} from "../../PageTitle";
-import {ExperienceSection} from "./ExperienceSection";
+import React, { Component } from "react"
+import { LineAnimation } from "./LineAnimation";
+import { PageTitle } from "../../PageTitle";
+import { ExperienceSection } from "./ExperienceSection";
 import Typography from "@mui/material/Typography";
-import {Anchor} from "../../Anchor";
+import { Anchor } from "../../Anchor";
 import NoSsr from "@mui/material/NoSsr";
-import {Helmet} from "react-helmet";
+import { Helmet } from "react-helmet";
 
 export default class Experience extends Component {
     constructor(props) {
@@ -13,21 +13,66 @@ export default class Experience extends Component {
     }
 
     render() {
-        const Line = () => <hr style={{borderColor:"#f50057"}}/>
-        const Paragraph = (props) => <div style={{paddingBottom:40}}>{props.children}</div>
+        const Line = () => <hr style={{ borderColor: "#f50057" }} />
+        const Paragraph = (props) => <div style={{ paddingBottom: 40 }}>{props.children}</div>
         return <>
             <Helmet>
                 <title>Pavel Vjalicin - Work Experience and Education</title>
                 <meta name={"description"} content={"Pavel Vjalicin - Work experience and education"} />
             </Helmet>
             <NoSsr>
-                <LineAnimation/>
+                <LineAnimation />
             </NoSsr>
             <PageTitle>Experience</PageTitle>
+            <ExperienceSection from={"August 2020"}
+                to={"Present"}
+                company={"White Bullet Solutions Limited"}
+                position={"Full-Stack Software Engineer (Contract)"}>
+                <Typography>
+                    Worked with product owners to design and implement new features and modernise old ones across the whole stack.
+                </Typography>
+                <br/>
+                <Typography>
+                    Developed a custom Android OS, designed for automated crawling of applications with physical devices. Created a scalable method of installing custom OS across hundreds of Pixel 2 devices.
+                </Typography>
+                <br/>
+                <Typography>
+                    Extensive work with AWS using web console, Java, Scala, JavaScript. Worked on performance diagnostics, cost optimisations. Worked with EC2, SQS, Lambda, CloudWatch, S3, RDS, AMI, ECS, CodeCommit, Spot, SNS, DynamoDB.
+                </Typography>
+                <br/>
+                <Typography>
+                    Found and achieved 100x performance improvements on multiple services with architecture changes.
+                </Typography>
+                <br/>
+                <Typography>
+                    Worked together with an AI contractor to integrate an Image Classification AI tool with existing codebase in a scalable, robust and maintainable way.
+                </Typography>
+                <br/>
+                <Typography>
+                    Introduced a modern front-end development environment for White Bullet. TypeScript, React, modern JavaScript,  testing environment. Replaced existing internal UI tools with more maintainable, more  functional, faster versions.
+                </Typography>
+                <br/>
+                <Typography>
+                    Introduced additional CI/CD pipelines for some of the services. (GoCD)
+                </Typography>
+                <br/>
+                <Typography>
+                    Introduced additional automated testing.
+                </Typography>
+                <br/>
+                <Typography>
+                    Stack: Scala, Play Framework, AWS,  Docker, Java, React, TypeScript, Postgresql
+                </Typography>
+            </ExperienceSection>
+            <ExperienceSection from={"August 2020"}
+                to={"Present"}
+                company={"Vjalicin Technologies LTD"}
+                position={"Managing Director"}>
+            </ExperienceSection>
             <ExperienceSection from={"June 2017"}
-                               to={"Present"}
-                               company={"Softar Consulting Limited"}
-                               position={"Lead Software Engineer"}>
+                to={"August 2020"}
+                company={"Softar Consulting Limited"}
+                position={"Lead Software Engineer"}>
                 <Typography>
                     Responsible for design and implementation of Softar's web development environment.
                 </Typography>
@@ -40,52 +85,52 @@ export default class Experience extends Component {
                 <Typography>
                     Worked with clients to design and implement software based business solutions.
                 </Typography>
-                <br/>
+                <br />
                 <Typography>
                     Worked with: React (JavaScript), Play Framework (Scala), Symfony (PHP), ASP.NET / Core (C#),
                     MariaDB, OpenAPI.
                 </Typography>
             </ExperienceSection>
             <ExperienceSection from={"2014"}
-                               to={"2017"}
-                               position={"Freelance Work"}>
+                to={"2017"}
+                position={"Freelance Work"}>
                 <Typography>
                     Worked with local businesses to resolve a wide-range of
                     IT/Graphics&nbsp;Design/Software challenges.
                     Primarily focusing on tasks with highly specialised skill requirements.
                 </Typography>
-                <br/>
+                <br />
                 <Typography>
                     Worked on: interactive online advertisements, company logos, business cards, posters.
                     Developed a wide variety of Apps based on client needs.
                 </Typography>
             </ExperienceSection>
             <ExperienceSection from={"2007"}
-                               to={"2016"}
-                               position={"Independent Game Developer"}>
+                to={"2016"}
+                position={"Independent Game Developer"}>
                 <Typography>
                     Developed simple video games for video game websites/mobile phones.
                     Negotiated video game sponsorship deals.
                     Worked with small teams of video game developers.
                     Lead end-to-end production.
                 </Typography>
-                <br/>
+                <br />
                 <Typography>
                     Technologies used: Adobe Flash (ActionScript) and Unity3D (C#).
                 </Typography>
             </ExperienceSection>
             <Paragraph>
                 <Typography variant={"h5"}>Education</Typography>
-                <Line/>
+                <Line />
                 <Typography>Institute for Apprenticeships & Technical Education Software Developer Level 4 (2019)</Typography>
-                <div style={{paddingLeft:20}}>
+                <div style={{ paddingLeft: 20 }}>
                     <Typography>
                         <Anchor href={"public/pv_qa_portfolio_clean.pdf"}>Apprenticeship Portfolio</Anchor>
                     </Typography>
                     <Typography>
                         Synoptic Project (C#&nbsp;ASP.NET&nbsp;Core&nbsp;REST&nbsp;API)
                     </Typography>
-                    <div style={{paddingLeft:20}}>
+                    <div style={{ paddingLeft: 20 }}>
                         <Typography>
                             <Anchor href={"public/pv_qa_synoptic_project_design.pdf"}>Design&nbsp;Document</Anchor>
                         </Typography>
@@ -98,7 +143,7 @@ export default class Experience extends Component {
             </Paragraph>
             <Paragraph>
                 <Typography variant={"h5"}>Certifications</Typography>
-                <Line/>
+                <Line />
                 <Typography>BCS Level 4 Diploma in Software Languages (2019)</Typography>
                 <Typography>BCS Certificate in Systems Development Essentials (2018)</Typography>
 
